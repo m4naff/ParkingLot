a791300a40168071e038cfa5fb09b87b8948ecb1
themanaff <ahmedovmenaf@gmail.com>
2024-10-12 22:14:45 +0400

    CustomUserDetails is ready

--- src/main/java/com/parkinglot/security/CustomUserDetails.java
+++ src/main/java/com/parkinglot/security/CustomUserDetails.java
@@ -1,5 +1,6 @@
 package com.parkinglot.security;
 
+import com.parkinglot.security.model.entity.UserEntity;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
@@ -7,10 +8,11 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serial;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+/**
+ * Custom implementation of UserDetails named {@link CustomUserDetails} of Spring Security UserDetails.
+ */
 @Getter
 @RequiredArgsConstructor
 public class CustomUserDetails implements UserDetails {
@@ -20,38 +22,100 @@ public class CustomUserDetails implements UserDetails {
 
     private final UserEntity userEntity;
 
+    /**
+     * Retrieves the authorities granted to the user.
+     *
+     * @return A collection of granted authorities.
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.singleton(new SimpleGrantedAuthority(userEntity.getRole().name()));
     }
 
+    /**
+     * Retrieves the password used to authenticate the user.
+     *
+     * @return The password.
+     */
     @Override
     public String getPassword() {
-        return "";
+        return userEntity.getPassword();
     }
 
+    /**
+     * Retrieves the username used to authenticate the user.
+     *
+     * @return The username.
+     */
     @Override
     public String getUsername() {
-        return "";
+        return userEntity.getUserName();
     }
 
+    /**
+     * Indicates whether the user's account has expired.
+     *
+     * @return Always returns true.
+     */
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
+    /**
+     * Indicates whether the user is locked or unlocked.
+     *
+     * @return Always returns true.
+     */
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
+    /**
+     * Indicates whether the user's credentials (password) has expired.
+     *
+     * @return Always returns true.
+     */
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
+    /**
+     * Indicates whether the user is enabled or disabled.
+     *
+     * @return Always returns true.
+     */
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
+    }
+
+    /**
+     * Retrieves the user's email.
+     *
+     * @return The user's email.
+     */
+    public String getEmail() {
+        return userEntity.getEmail();
+    }
+
+    /**
+     * Retrieves the user's ID.
+     *
+     * @return The user's ID.
+     */
+    public String getId() {
+        return userEntity.getId();
+    }
+
+    /**
+     * Retrieves the user's claims.
+     *
+     * @return A map containing the user's claims.
+     */
+    public Map<String, Object> getClaims() {
+        return userEntity.getClaims();
     }
 }
--- src/main/java/com/parkinglot/security/model/entity/UserEntity.java
+++ src/main/java/com/parkinglot/security/model/entity/UserEntity.java
@@ -1,11 +1,17 @@
 package com.parkinglot.security.model.entity;
 
+import com.parkinglot.model.entity.VehicleEntity;
 import com.parkinglot.model.enums.Role;
+import com.parkinglot.model.enums.TokenClaims;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Represents a user entity named {@link UserEntity}.
@@ -39,5 +45,25 @@ public class UserEntity {
     @Column(name = "ROLE")
     private Role role;
 
+    @OneToMany(
+            fetch = FetchType.LAZY,
+            mappedBy = "userEntity"
+    )
+    private List<VehicleEntity> vehicleEntities;
+
+    /**
+     * Generates JWT claims for the user.
+     *
+     * @return Map of JWT claims.
+     */
+    public Map<String, Object> getClaims() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(TokenClaims.ID.getValue(),this.id);
+        claims.put(TokenClaims.USERNAME.getValue(),this.userName);
+        claims.put(TokenClaims.ROLES.getValue(),this.role);
+        claims.put(TokenClaims.USER_FULL_NAME.getValue(),this.fullName);
+        claims.put(TokenClaims.EMAIL.getValue(),this.email);
+        return claims;
+    }
 
 }
