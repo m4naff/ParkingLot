15efa1dd9af1f5910576a41983fc34b025c84b4a
themanaff <ahmedovmenaf@gmail.com>
2024-10-19 18:54:36 +0400

    VehicleService implementation has finished

---
+++ src/main/java/com/parkinglot/exception/vehicle/VehicleAlreadyExist.java
@@ -0,0 +1,23 @@
+package com.parkinglot.exception.vehicle;
+
+import java.io.Serial;
+
+/**
+ * Exception class named {@link VehicleAlreadyExist} thrown when a user tries to add a vehicle that already exists.
+ */
+public class VehicleAlreadyExist extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 5431951469912355123L;
+
+    private static final String DEFAULT_MESSAGE =
+            "The User already has this vehicle";
+
+    public VehicleAlreadyExist() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public VehicleAlreadyExist(final String message) {
+        super(DEFAULT_MESSAGE + " " + message);
+    }
+}
--- src/main/java/com/parkinglot/service/vehicle/impl/VehicleServiceImpl.java
+++ src/main/java/com/parkinglot/service/vehicle/impl/VehicleServiceImpl.java
@@ -1,7 +1,11 @@
 package com.parkinglot.service.vehicle.impl;
 
+import com.parkinglot.exception.user.UserNotFoundException;
+import com.parkinglot.exception.vehicle.VehicleAlreadyExist;
+import com.parkinglot.exception.vehicle.VehicleNotFoundException;
 import com.parkinglot.model.Vehicle;
 import com.parkinglot.model.dto.request.vehicle.VehicleRequest;
+import com.parkinglot.model.dto.response.ParkDetailResponse;
 import com.parkinglot.model.dto.response.VehicleParkingDetailResponse;
 import com.parkinglot.model.entity.VehicleEntity;
 import com.parkinglot.model.mapper.park.ParkEntityToParkDetailResponse;
@@ -9,10 +13,16 @@ import com.parkinglot.model.mapper.vehicle.VehicleEntityToVehicleMapper;
 import com.parkinglot.model.mapper.vehicle.VehicleRequestToVehicleMapper;
 import com.parkinglot.model.mapper.vehicle.VehicleToVehicleEntityMapper;
 import com.parkinglot.repository.VehicleRepository;
+import com.parkinglot.security.model.entity.UserEntity;
 import com.parkinglot.service.auth.UserService;
 import com.parkinglot.service.vehicle.VehicleService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Service implementation class named {@link VehicleServiceImpl} for managing vehicle operations.
@@ -37,23 +47,107 @@ public class VehicleServiceImpl implements VehicleService {
     private final VehicleEntityToVehicleMapper vehicleEntityToVehicleMapper =
             VehicleEntityToVehicleMapper.initialize();
 
+    /**
+     * Assigns a vehicle to a user.
+     *
+     * @param id             the ID of the user
+     * @param vehicleRequest the vehicle details
+     * @return the assigned vehicle
+     */
     @Override
-    public Vehicle assignVehicleToUser(String id, VehicleRequest vehicleRequest) {
-        return null;
+    @Transactional
+    public Vehicle assignVehicleToUser(final String id, final VehicleRequest vehicleRequest) {
+        final UserEntity userEntity = userService.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+
+        final Vehicle vehicle = vehicleRequestToVehicleMapper.map(vehicleRequest);
+
+        final VehicleEntity vehicleEntity = assignUserToVehicle(userEntity, vehicle);
+
+        return vehicleEntityToVehicleMapper.map(vehicleEntity);
     }
 
+    /**
+     * Assigns a user to a vehicle and saves the vehicle entity.
+     *
+     * @param userEntity the user entity to assign to the vehicle
+     * @param vehicle    the vehicle to assign the user to
+     * @return the persisted vehicle entity with the assigned user
+     * @throws VehicleAlreadyExist if a vehicle with the same license plate already exists
+     */
+    private VehicleEntity assignUserToVehicle(final UserEntity userEntity, final Vehicle vehicle) {
+        existsByLicensePlate(vehicle);
+
+        final VehicleEntity vehicleEntityToBePersist = vehicleToVehicleEntityMapper.map(vehicle);
+
+        vehicleEntityToBePersist.setUserEntity(userEntity);
+
+        vehicleRepository.save(vehicleEntityToBePersist);
+
+        return vehicleEntityToBePersist;
+    }
+
+    /**
+     * Checks if a vehicle with the given license plate already exists.
+     *
+     * @param vehicle the vehicle to check
+     * @throws VehicleAlreadyExist if a vehicle with the same license plate already exists
+     */
+    private void existsByLicensePlate(final Vehicle vehicle) {
+        if(Boolean.TRUE.equals(vehicleRepository.existsByLicensePlate(vehicle.getLicensePlate()))) {
+            throw new VehicleAlreadyExist();
+        }
+    }
+
+    /**
+     * Assigns a vehicle to a user if not assigned already; otherwise, retrieves the assigned vehicle.
+     *
+     * @param userId         the ID of the user
+     * @param vehicleRequest the vehicle details
+     * @return the assigned or retrieved vehicle
+     */
     @Override
+    @Transactional
     public Vehicle assignOrGet(String userId, VehicleRequest vehicleRequest) {
-        return null;
+        final Optional<VehicleEntity> existingVehicle = vehicleRepository.findByLicensePlate(vehicleRequest.getLicensePlate());
+        final VehicleEntity vehicleEntity = existingVehicle.orElseGet(() -> {
+            Vehicle assignedVehicleEntity = assignVehicleToUser(userId, vehicleRequest);
+            return vehicleToVehicleEntityMapper.map(assignedVehicleEntity);
+        });
+        return vehicleEntityToVehicleMapper.map(vehicleEntity);
     }
 
+    /**
+     * Retrieves a vehicle entity by its license plate.
+     *
+     * @param licensePlate the license plate of the vehicle
+     * @return the vehicle entity
+     */
     @Override
-    public VehicleEntity findByLicensePlate(String licensePlate) {
-        return null;
+    public VehicleEntity findByLicensePlate(final String licensePlate) {
+        return vehicleRepository
+                .findByLicensePlate(licensePlate)
+                .orElseThrow(VehicleNotFoundException::new);
     }
 
+    /**
+     * Retrieves parking details of a vehicle by its license plate.
+     *
+     * @param licensePlate the license plate of the vehicle
+     * @return the parking details of the vehicle
+     */
     @Override
-    public VehicleParkingDetailResponse getParkingDetails(String licensePlate) {
-        return null;
+    public VehicleParkingDetailResponse getParkingDetails(final String licensePlate) {
+        final VehicleEntity vehicleEntity = vehicleRepository.findByLicensePlate(licensePlate)
+                .orElseThrow(() -> new VehicleNotFoundException(licensePlate));
+
+        final List<ParkDetailResponse> parkDetails = vehicleEntity.getParkEntities().stream()
+                .map(parkEntityToParkDetailResponse::map)
+                .toList();
+
+        return VehicleParkingDetailResponse.builder()
+                .licensePlate(vehicleEntity.getLicensePlate())
+                .parkDetails(parkDetails)
+                .build();
     }
 }
