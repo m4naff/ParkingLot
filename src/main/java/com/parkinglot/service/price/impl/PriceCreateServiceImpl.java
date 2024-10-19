bf40d3f760630fa8fbde6d6ec687f4f9f9895ef3
themanaff <ahmedovmenaf@gmail.com>
2024-10-19 19:15:43 +0400

    PriceCreteService implementation has finished

--- src/main/java/com/parkinglot/service/price/impl/PriceCreateServiceImpl.java
+++ src/main/java/com/parkinglot/service/price/impl/PriceCreateServiceImpl.java
@@ -3,6 +3,9 @@ package com.parkinglot.service.price.impl;
 import com.parkinglot.model.Price;
 import com.parkinglot.model.PriceList;
 import com.parkinglot.model.dto.request.price.PriceCreateRequest;
+import com.parkinglot.model.entity.PriceEntity;
+import com.parkinglot.model.mapper.price.PriceDTOMapper;
+import com.parkinglot.model.mapper.price.PriceMapper;
 import com.parkinglot.repository.PriceRepository;
 import com.parkinglot.service.price.PriceCreateService;
 import lombok.RequiredArgsConstructor;
@@ -17,8 +20,17 @@ public class PriceCreateServiceImpl implements PriceCreateService {
 
     private final PriceRepository priceRepository;
 
+    /**
+     * Creates a new price.
+     *
+     * @param priceList the price list to which the price belongs
+     * @param priceCreateRequest the request containing the price details
+     * @return the created price
+     */
     @Override
-    public Price creatPrice(PriceList priceList, PriceCreateRequest priceCreateRequest) {
-        return null;
+    public Price creatPrice(final PriceList priceList, final PriceCreateRequest priceCreateRequest) {
+        PriceEntity priceEntityToBeSave = PriceDTOMapper.mapForSaving(priceCreateRequest);
+        priceRepository.save(priceEntityToBeSave);
+        return PriceMapper.toDomainModel(priceEntityToBeSave);
     }
 }
